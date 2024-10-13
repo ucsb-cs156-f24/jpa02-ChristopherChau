2dd829d9e94e82341896caac71de641894326a52
Christopher Chau <christopherchau@ucsb.edu>
2024-10-12 20:07:19 -0700

    cc - testing gap in equals 3.3 done

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -19,8 +19,70 @@ public class TeamTest {
        assert(team.getName().equals("test-team"));
     }
 
+
    
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
+    @Test
+    public void test_equal_same_object() {
+        assertEquals(true, team.equals(team));
+    }
 
+    @Test
+    public void test_equal_different_class() {
+        String notATeam = "Not a team";
+        assertEquals(false, team.equals(notATeam));
+    }
+
+    @Test
+    public void test_equal_same_name_and_members() {
+        Team team1 = new Team("Team1");
+        team1.addMember("Eugene");
+        team1.addMember("Chris");
+
+        Team team2 = new Team("Team1");
+        team2.addMember("Eugene");
+        team2.addMember("Chris");
+
+        assertEquals(true, team1.equals(team2));
+    }
+
+    @Test
+    public void test_equal_same_name_different_members() {
+        Team team1 = new Team("Team1");
+        team1.addMember("Eugene");
+        team1.addMember("Chris");
+
+        Team team2 = new Team("Team1");
+        team2.addMember("Member1");
+        team2.addMember("Member2");
+
+        assertEquals(false, team1.equals(team2));
+    }
+
+    @Test
+    public void test_equal_different_name_same_members() {
+        Team team1 = new Team("Team1");
+        team1.addMember("Eugene");
+        team1.addMember("Chris");
+
+        Team team2 = new Team("Team2");
+        team2.addMember("Eugene");
+        team2.addMember("Chris");
+
+        assertEquals(false, team1.equals(team2));
+    }
+
+    @Test
+    public void test_equal_different_name_and_members() {
+        Team team1 = new Team("Team1");
+        team1.addMember("Eugene");
+        team1.addMember("Chris");
+
+        Team team2 = new Team("Team2");
+        team2.addMember("Member1");
+        team2.addMember("Member2");
+
+        assertEquals(false, team1.equals(team2));
+    }
 }
